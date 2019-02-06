371b4cca253c4f7c5f533aba92f533511dee714d
Francisca Parada <francisca.parada@MacBook-Pro-de-Francisca.local>
2019-02-06 11:51:34 -0300

    BookDaoImpl fixed up

--- src/com/twu/dao/BookDaoImpl.java
+++ src/com/twu/dao/BookDaoImpl.java
@@ -11,14 +11,11 @@ public class BookDaoImpl implements IBookDao {
     List<BookDto> books;
 
     public BookDaoImpl(){
-
         books = new ArrayList<BookDto>();
         BookDto book1 = new BookDto("Apologia Socrates", -500, "Platón", true);
         BookDto book2 = new BookDto("El Capital", 1932, "Karl Marx", true);
         BookDto book3 = new BookDto("El ser y el tiempo", 1954, "Martin Heidegger", true);
         BookDto book4 = new BookDto("1984", 1949, "George Orwell", false);
-
-
         books.add(book1);
         books.add(book2);
         books.add(book3);
@@ -34,57 +31,42 @@ public class BookDaoImpl implements IBookDao {
     }
 
     public List<String> showBooks(){
-
         List<String> bookList = new ArrayList<String>();
-
         for (int i=INITIAL_VALUE; i<books.size(); i++){
-
             int num = i+1;
 
-            //TODO: revisar formateo de strings en java
-            String book = "\n"+num+".- "+books.get(i).getBookName()+" - " +books.get(i).getAutor()+" - "+books.get(i).getYearPublisher() + " \n";
-
             if (books.get(i).isState()){
-
+                String book = "\n"+num+".- "+books.get(i).getBookName()+" - " +books.get(i).getAutor()+" - "+books.get(i).getYearPublisher() + " \n";
                 bookList.add(book);
             }
-
         }
-
         return bookList;
-
     }
 
     public void returnBookByName(String bookName){
-
-        for (int i = INITIAL_VALUE; i<books.size(); i++){
-
-            if (books.get(i).getBookName().equalsIgnoreCase(bookName)){
-                books.get(i).setState(true);
+        for (int index = INITIAL_VALUE; index<books.size(); index++){
+            if (books.get(index).getBookName().equalsIgnoreCase(bookName)){
+                books.get(index).setState(true);
             }
         }
     }
 
     public void selectBookByName(String bookName){
-        //TODO: hacerle caso a intelliJ a veces, revisar foreach
-
-        for (int i=INITIAL_VALUE; i<books.size(); i++){
-            if (books.get(i).getBookName().equalsIgnoreCase(bookName)){
-                books.get(i).setState(false);
+        for (int index=INITIAL_VALUE; index<books.size(); index++){
+            if (books.get(index).getBookName().equalsIgnoreCase(bookName)){
+                books.get(index).setState(false);
             }
         }
     }
 
     public boolean checkBookName(String bookName){
-
-        for (int i=INITIAL_VALUE; i<books.size(); i++){
-            if (books.get(i).getBookName().equalsIgnoreCase(bookName)){
+        for (int index=INITIAL_VALUE; index<books.size(); index++){
+            if (books.get(index).getBookName().equalsIgnoreCase(bookName)){
                return true;
             }
         }
         return false;
     }
-
 
 
 }
--- test/com/twu/dao/BookDaoImplTest.java
+++ test/com/twu/dao/BookDaoImplTest.java
@@ -67,7 +67,6 @@ public class BookDaoImplTest {
         bookDao.selectBookByName("nn");
 
         assertEquals(bookTest.isState(), false);
-
     }
 
     @Test
